7c7a9c20fbe728e292fd996f714057d2f34dba51
Pratham3168 <prathamtanwar593@gmail.com>
2026-04-16 00:27:35 +0530

    Add implementations for Morris Inorder and Preorder Traversal

---
+++ Binary Search Trees/MorrisInorder.cpp
@@ -0,0 +1,28 @@
+// //MORRIS INORDER TRAVERSAL
+
+// vector<int> getInorder(TreeNode* root){
+//     vector<int> inorder;
+//     TreeNode* curr = root;
+//     while(curr != NULL){
+//         if(curr->left == NULL){
+//             inorder.push_back(curr->val);
+//             curr = curr->right;
+//         }
+//         else{
+//             TreeNode* prev = curr->left;
+//             while(prev->right && prev->right != curr){
+//                 prev = prev ->right;
+//             }
+//             if(prev->right == NULL){
+//                 prev ->right = curr;
+//                 curr = curr->left;
+//             }
+//             else{
+//                 prev->right = NULL;
+//                 inorder.push_back(curr->val);
+//                 curr = curr->right;
+//             }
+//         }
+//     }
+//     return inorder;
+// }
---
+++ Binary Search Trees/MorrisPreorder.cpp
@@ -0,0 +1,28 @@
+// //MORRIS PREORDER TRAVERSAL
+
+// vector<int> getPreorder(TreeNode* root){
+//     vector<int> preorder;
+//     TreeNode* curr = root;
+//     while(curr != NULL){
+//         if(curr->left == NULL){
+//             preorder.push_back(curr->val);
+//             curr = curr->right;
+//         }
+//         else{
+//             TreeNode* prev = curr->left;
+//             while(prev->right && prev->right != curr){
+//                 prev = prev ->right;
+//             }
+//             if(prev->right == NULL){
+//                 prev ->right = curr;
+//                 preorder.push_back(curr->val);
+//                 curr = curr->left;
+//             }
+//             else{
+//                 prev->right = NULL;
+//                 curr = curr->right;
+//             }
+//         }
+//     }
+//     return inorder;
+// }
