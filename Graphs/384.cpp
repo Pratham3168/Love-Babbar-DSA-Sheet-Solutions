de472f67dd47e404166c3203f697878d453a8b89
Pratham3168 <prathamtanwar593@gmail.com>
2026-08-13 01:40:00 +0530

    Add implementation of Bellman-Ford algorithm for detecting negative weight cycles in a graph

---
+++ Graphs/384.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isNegativeWeightCycle(int V, vector<vector<int>>& edges) {
+        
+        vector<int> dist(V, 0);
+
+        // Relax all edges V-1 times
+        for(int i = 0; i < V - 1; i++) {
+            
+            for(int j = 0; j < edges.size(); j++) {
+                
+                int u = edges[j][0];
+                int v = edges[j][1];
+                int w = edges[j][2];
+                
+                if(dist[u] + w < dist[v]) {
+                    dist[v] = dist[u] + w;
+                }
+            }
+        }
+
+        // Vth relaxation
+        // If any distance still decreases,
+        // negative weight cycle exists
+        for(int j = 0; j < edges.size(); j++) {
+            
+            int u = edges[j][0];
+            int v = edges[j][1];
+            int w = edges[j][2];
+            
+            if(dist[u] + w < dist[v]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
